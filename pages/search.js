83b85bcf65eb19086d85d0838acef5687a1c3112
Bukhosi Moyo <bukhosizimcode@gmail.com>
2021-04-21 15:43:35 +0200

    ✨ : Added the search page component

---
+++ pages/search.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import Header from "../components/Header"
+
+function Search() {
+    return (
+        <div>
+            <Head>
+                <title>Search</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            {/* Header */}
+
+            <Header />
+
+            {/* Search Results */}
+            
+        </div>
+    );
+}
+export default Search;
