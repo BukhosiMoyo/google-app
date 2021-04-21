2687c6e13be2825af8608dc6b75e7401a00168f9
Bukhosi Moyo <bukhosizimcode@gmail.com>
2021-04-21 15:42:22 +0200

    ✨ : Added the search page and itd functionalities

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
-import Head from 'next/head'
-import Avatar from '../components/Avatar'
-import Footer from '../components/Footer'
-import {MicrophoneIcon, ViewGridIcon} from "@heroicons/react/solid";
+import Head from "next/head";
+import Avatar from "../components/Avatar";
+import Footer from "../components/Footer";
+import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid";
 import Image from "next/image/";
-import {SearchIcon} from "@heroicons/react/outline";
+import { SearchIcon } from "@heroicons/react/outline";
+import { useRef } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
 
   const search = (e) => {
     e.preventDefault();
+    const term = searchInputRef.current.value;
+
+    if (!term) return;
+    router.push(`/search?term=${term}`);
   };
 
   return (
@@ -21,7 +29,6 @@ export default function Home() {
       {/* Header */}
 
       <header className="flex w-full py-3 px-6 justify-between text-sm text-gray-700">
-
         {/* Left */}
         <div className="flex space-x-4 items-center">
           <p className="link">About</p>
@@ -34,50 +41,58 @@ export default function Home() {
           <p className="link">Images</p>
 
           {/* Icon */}
-          <ViewGridIcon className="h-10 w-10 p-2 rounded-full
+          <ViewGridIcon
+            className="h-10 w-10 p-2 rounded-full
           hover:bg-gray-100 cursor-pointer transform hover:scale-110
-          transtion duration-200 " />
+          transtion duration-200 "
+          />
 
           {/* Avatar */}
 
           <Avatar url="https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png" />
-
         </div>
       </header>
-      
+
       {/* Body */}
 
       <form className="flex flex-col items-center mt-44 flex-grow w-4/5">
-        <Image 
-        src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
-        height={100}
-        width={300} />
+        <Image
+          src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+          height={100}
+          width={300}
+        />
 
-        <div className="flex w-full mt-5 hover:shadow-lg
+        <div
+          className="flex w-full mt-5 hover:shadow-lg
          focus-within:shadow-lg max-w-md rounded-full border 
-         border-gray-200 px-5 py-3 items-center sm:max-w-lg lg:max-w-2xl">
+         border-gray-200 px-5 py-3 items-center sm:max-w-lg lg:max-w-2xl"
+        >
           <SearchIcon className="h-4 mr-3 text-gray-500" />
-          <input type="text" className="flex-grow focus:outline-none"/>
-          
-          <Image 
+          <input ref={searchInputRef} type="text" className="flex-grow focus:outline-none" />
+
+          <Image
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Google_mic.svg/1200px-Google_mic.svg.png"
             height={20}
-            width={14} />
-
+            width={14}
+          />
         </div>
 
-        <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 
-        sm:-space-y-0 sm:flex-row sm:space-x-4">
-          <button onClick={search} className="btn">Google Search</button>
+        <div
+          className="flex flex-col w-1/2 space-y-2 justify-center mt-8 
+        sm:-space-y-0 sm:flex-row sm:space-x-4"
+        >
+          <button onClick={search} className="btn">
+            Google Search
+          </button>
 
-          <button onClick={search} className="btn">I'm Feeling Lucky</button>
+          <button onClick={search} className="btn">
+            I'm Feeling Lucky
+          </button>
         </div>
-
       </form>
 
       {/* Footer */}
       <Footer />
-
     </div>
-  )
+  );
 }
