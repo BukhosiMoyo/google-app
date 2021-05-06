cb92eedda40c51baed228792d8fb3311cadde584
Bukhosi Moyo <bukhosizimcode@gmail.com>
2021-05-06 05:36:56 +0200

    💄 : Prioritizing styles for search page Avatar alignment.

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,8 +1,8 @@
-function Avatar({url}) {
+function Avatar({url, className}) {
     return <img loading="lazy" 
-    className="h-10 rounded-full
+    className={`h-10 rounded-full
     cursor-pointer transtion duration-200
-    transform hover:scale-110"
+    transform hover:scale-110 ${className}`}
     src={url} 
     alt="profile picture" />;
     
