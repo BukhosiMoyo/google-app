49f9843907363bb561ca9208e372a0427d220361
Bukhosi Moyo <bukhosizimcode@gmail.com>
2021-05-06 05:40:17 +0200

    ✨ : Adding server side rendering to the seach page

--- pages/search.js
+++ pages/search.js
@@ -1,21 +1,47 @@
 import Head from "next/head";
-import Header from "../components/Header"
+import Header from "../components/Header";
+import { API_KEY, CONTEXT_KEY } from "../keys";
+import Response from "../Response"
+import {useRouter} from "next/router";
+import SearchResults from "../components/SearchResults"
 
-function Search() {
+
+function Search({ results }) {
+    const router = useRouter();
+
+    console.log(results);
     return (
         <div>
-            <Head>
-                <title>Search</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+        <Head>
+            <title>{router.query.term} - Google Search</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-            {/* Header */}
+        {/* Header */}
 
-            <Header />
+        <Header />
 
-            {/* Search Results */}
-            
+        {/* Search Results */}
+        <SearchResults results={results}/>
         </div>
     );
 }
 export default Search;
+
+export async function getServerSideProps(context) {
+  const useDummyData = false;
+  const startIndex = context.query.start || "0";
+
+  const data = useDummyData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+      ).then((response) => response.json());
+
+  // After the SERVER has rendered... pass the result to the client.
+  return {
+    props: {
+      results: data,
+    },
+  };
+}
