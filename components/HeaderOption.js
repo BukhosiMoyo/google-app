ae32399d052a130795e0304fe46e3d364a7b0b29
Bukhosi Moyo <bukhosizimcode@gmail.com>
2021-05-06 05:43:00 +0200

    ✨ : Adding header option for google category options

---
+++ components/HeaderOption.js
@@ -0,0 +1,15 @@
+function HeaderOption({Icon, title, selected}) {
+    return <div>
+        {/* Left */}
+        
+        {/* Right */}
+        <div className={`flex items-center space-x-1 border-b-4 border-transparent hover:text-blue-500 hover:border-blue-500 pb-4 cursor-pointer ${ selected && "text-blue-500 border-blue-500"}`}>
+            <Icon className="h-4" />
+            <p className="hidden sm:inline-flex">{title}</p>
+           
+        </div>
+    </div>;
+};
+
+export default HeaderOption;
+
